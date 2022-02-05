fa684a7799c11930a9bf788fdcc3710e5dba63d7
Gustavo <gustavobernardo094@gmail.com>
2022-02-05 10:23:54 -0300

    Working on the OdinProject looping exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let string2 = string;
+  let i = 1 ;
+  if (num === - 1){
+    string2 = 'ERROR';
+    return string2;
+  }
+  else if (num === 0){
+    string2 = ''
+    return string2;
+  }else{
+    while (i < num) {
+      // string;
+      string2 += string;
+      console.log(i, string2);
+      i++;
+    }
+  }
+  return string2;
 };
-
+repeatString('hey', 3);
 // Do not edit below this line
 module.exports = repeatString;
