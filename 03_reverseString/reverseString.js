8294fbf05aa85392cd6b35b4db02233b717eebaa
Gustavo <gustavobernardo094@gmail.com>
2022-02-05 17:25:18 -0300

    Writing a function called "removeFromString". Which remove a string from an array

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
+let stringSplit;
+      
 
+const reverseString = function(string) {
+  if (string === ''){
+    return '';
+  }else{
+    stringSplit = string.split('')
+    let myArray = [];
+    
+    for (let i = stringSplit.length - 1; i > -1; i--){
+      
+      // myArray.push(string[i])
+      myArray += string[i];
+      // console.log(myArray) 
+    }
+    return myArray
+  }
 };
-
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, remove){
+  const myArray = [];
+  let i = 0;
+  let index = array.indexOf(remove);
+  while (i < array.length){
+    if(array[i] === remove){
+      // console.log(i);
+      // console.log('I find you')
+      (array.splice(i, 1))
+      // break;
+    }else{
+      i++;
+      // console.log(i);
+    }
+  }
+  // myArray.push(array);
 
-};
+  return array
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
