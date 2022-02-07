9aef62d765dbae25defe3f8e3298676c80b9a0af
Gustavo <gustavobernardo094@gmail.com>
2022-02-07 08:48:45 -0300

    making a function that takes an array and some other arguments then removes the other arguments from that array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,22 +1,25 @@
-const removeFromArray = function(array, remove){
+const removeFromArray = function(array, ...remove){
+  // console.log(remove[0])
+  // console.log(remove[1])
+  // console.log(remove[2])
+  // console.log(remove[3])
+
   const myArray = [];
   let i = 0;
-  let index = array.indexOf(remove);
+  // let index = array.indexOf(remove[0]);
+  // console.log('index',index)
   while (i < array.length){
-    if(array[i] === remove){
-      // console.log(i);
-      // console.log('I find you')
+    if(array[i] === remove[0]|| array[i] === remove[1]||array[i] === remove[2]||array[i] === remove[3]){
       (array.splice(i, 1))
-      // break;
     }else{
       i++;
-      // console.log(i);
     }
   }
-  // myArray.push(array);
+  myArray.push(array);
 
   return array
 }
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
